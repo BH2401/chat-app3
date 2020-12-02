8005c29df2ff60344089aafa6d0be168014b1dd9
BH2401 <zingigene@gmail.com>
2020-12-02 13:21:37 +0900

    各モデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    admin {'kk22'}
+  end
+end
